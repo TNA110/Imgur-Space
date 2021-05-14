2da3846c40275225010ecfce597230112d1f23d3
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-05-14 13:25:30 +0300

    Вынес функцию скачивания картинок Hubble

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,12 +1,7 @@
 import requests
 import requiered_func
 
-
-def fetch_hubble_images(collection_name, download_path):
-    response = requests.get(f"http://hubblesite.org/api/v3/images/{collection_name}")
-    response.raise_for_status()
-    collection = response.json()
-    for image_number, image in enumerate(collection):
+def fetch_best_quality_image(image_number, image, download_path):
         image_id = image.get("id")
         response = requests.get(f"http://hubblesite.org/api/v3/image/{image_id}", verify=False)
         response.raise_for_status()
@@ -17,3 +12,10 @@ def fetch_hubble_images(collection_name, download_path):
         image_extension = requiered_func.get_extension(image_url)
         filename = f"{image_number}{image_id}{image_extension}"
         requiered_func.download_image(image_url, filename, download_path)
+
+def fetch_hubble_images(collection_name, download_path):
+    response = requests.get(f"http://hubblesite.org/api/v3/images/{collection_name}")
+    response.raise_for_status()
+    collection = response.json()
+    for image_number, image in enumerate(collection):
+        fetch_best_quality_image(image_number, image, download_path)
